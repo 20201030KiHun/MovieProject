50358e5a7913e4b0ffb799af7c0d24a26f2878c0
20201030KiHun <164668212+20201030KiHun@users.noreply.github.com>
2024-04-30 22:45:50 +0900

    Delete test directory

--- test/milestone2/src/main/java/cse364/group10/project/Grade/GradeRepository.java
+++
@@ -1,7 +0,0 @@
-package cse364.group10.project.Grade;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface GradeRepository extends JpaRepository<Grade, Long> {
-
-}
--- test/run.sh
+++
@@ -1,5 +0,0 @@
-cd milestone2
-
-mvn package
-
-java -jar ./target/cse364-project-1.0-SNAPSHOT-with-dependencies.jar
