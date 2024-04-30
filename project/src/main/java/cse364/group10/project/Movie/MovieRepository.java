de50de3c6e178d0c6536dfbc1f72f2fcba9249b1
20201030KiHun <164668212+20201030KiHun@users.noreply.github.com>
2024-05-01 01:23:48 +0900

    Delete project directory

--- project/src/main/java/cse364/group10/project/Movie/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package cse364.group10.project.Movie;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import  java.util.List;
-
-public interface MovieRepository extends  JpaRepository<Movie, Long> {
-    //List<Movie> findByGenre(String genre);
-}
--- project/src/main/java/cse364/group10/project/User/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cse364.group10.project.User;
-
-public class UserNotFoundException extends  RuntimeException {
-    UserNotFoundException(Long id) {
-        super("Could not find user " + id);
-    }
-}
