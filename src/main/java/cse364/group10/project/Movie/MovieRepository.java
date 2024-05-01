ad108b96bc48bd9b4cfe1d9a6fd27d16ef3b978f
20201030KiHun <164668212+20201030KiHun@users.noreply.github.com>
2024-05-01 23:44:43 +0900

    Update MovieRepository.java

--- src/main/java/cse364/group10/project/Movie/MovieRepository.java
+++ src/main/java/cse364/group10/project/Movie/MovieRepository.java
@@ -1,8 +1,10 @@
 package cse364.group10.project.Movie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface MovieRepository extends  JpaRepository<Movie, Long> {
     //List<Movie> findByGenre(String genre);
 }
