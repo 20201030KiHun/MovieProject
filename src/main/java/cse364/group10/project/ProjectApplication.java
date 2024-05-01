df1a1253768ffa0e5a7c86cc92b2697f95863883
20201030KiHun <164668212+20201030KiHun@users.noreply.github.com>
2024-05-01 23:54:44 +0900

    Update ProjectApplication.java

--- src/main/java/cse364/group10/project/ProjectApplication.java
+++ src/main/java/cse364/group10/project/ProjectApplication.java
@@ -3,7 +3,11 @@ package cse364.group10.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan(basePackages = "cse364.group10.project.Movie")
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ProjectApplication {
 	public static void main(String[] args) {
