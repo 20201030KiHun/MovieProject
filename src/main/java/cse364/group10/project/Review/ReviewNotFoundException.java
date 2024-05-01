f8850eb1a334bf77f0a2907af15b1f8e6acd9457
20201030KiHun <164668212+20201030KiHun@users.noreply.github.com>
2024-05-01 19:01:40 +0900

    Delete src/main/java/cse364/group10/project/Review directory

--- src/main/java/cse364/group10/project/Review/ReviewNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cse364.group10.project.Review;
-
-public class ReviewNotFoundException extends RuntimeException {
-    ReviewNotFoundException(Long id) {
-        super("Could not find review " + id);
-    }
-}
--- src/main/java/cse364/group10/project/Review/ReviewRepository.java
+++
@@ -1,6 +0,0 @@
-package cse364.group10.project.Review;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
