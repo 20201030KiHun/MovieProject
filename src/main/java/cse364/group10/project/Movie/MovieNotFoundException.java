fba319068a122094c4dceb69660017dc84a38846
20201030KiHun <164668212+20201030KiHun@users.noreply.github.com>
2024-05-01 18:57:03 +0900

    Delete src/main/java/cse364/group10/project directory

--- src/main/java/cse364/group10/project/Movie/MovieNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cse364.group10.project.Movie;
-
-public class MovieNotFoundException extends RuntimeException {
-    MovieNotFoundException(Long id) {
-        super("Cound not find movie " + id);
-    }
-}
