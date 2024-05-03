1bc61aeac7a962f783d5d6da9f52c611cdc35ac3
20201030KiHun <164668212+20201030KiHun@users.noreply.github.com>
2024-05-03 19:35:29 +0900

    Update EvaluateGenre.java

--- src/main/java/cse364/group10/project/Rating/EvaluateGenre/EvaluateGenre.java
+++ src/main/java/cse364/group10/project/Rating/EvaluateGenre/EvaluateGenre.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Component
 public class EvaluateGenre {
