89c90203d4b156f80e65d9a2d63534211cc79647
20201030KiHun <164668212+20201030KiHun@users.noreply.github.com>
2024-05-03 20:10:18 +0900

    Update RatingRepository.java

--- src/main/java/cse364/group10/project/Rating/RatingRepository.java
+++ src/main/java/cse364/group10/project/Rating/RatingRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
     List<Rating> findByMovieName(String movieName);
+    List<Rating> findByGenreName(String genre);
 }
