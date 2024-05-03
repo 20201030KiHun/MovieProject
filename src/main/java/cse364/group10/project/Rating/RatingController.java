81635075f90c7a186d4b3aef6bda2b5d6f089422
20201030KiHun <164668212+20201030KiHun@users.noreply.github.com>
2024-05-03 18:54:45 +0900

    Update RatingController.java

--- src/main/java/cse364/group10/project/Rating/RatingController.java
+++ src/main/java/cse364/group10/project/Rating/RatingController.java
@@ -1,6 +1,7 @@
 package cse364.group10.project.Rating;
 
 import java.util.List;
+import cse364.group10.project.Rating.EvaluateGenre.EvaluateGenre;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/movies/{id}")
