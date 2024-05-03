c83bda0d445b00954ae67eb43879e7b420fe5baf
20201030KiHun <164668212+20201030KiHun@users.noreply.github.com>
2024-05-03 15:08:46 +0900

    Update and rename src/main/java/cse364/group10/project/Rating/RatingEvaluate.java to src/main/java/cse364/group10/project/Rating/Evaluate /EvaluateGenre.java

---
+++ src/main/java/cse364/group10/project/Rating/Evaluate /EvaluateGenre.java
@@ -0,0 +1,10 @@
+package cse364.group10.project.Rating.EvaluateGenre;
+
+import cse364.group10.project.Rating.Rating;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
--- src/main/java/cse364/group10/project/Rating/RatingEvaluate.java
+++
@@ -1,4 +0,0 @@
-package cse364.group10.project.Rating;
-
-import cse364.group10.project.User.User;
-import jakarta.persistence.*;
